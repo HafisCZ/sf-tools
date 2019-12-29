950a0c22790a1b3910b1d2f8e6a1d53b8615fa4c
Martin Halfar <HafisCZ@users.noreply.github.com>
2019-12-29 19:50:21 +0100

    Beta: Performance update

--- beta/js/core.js
+++ beta/js/core.js
@@ -24,6 +24,46 @@ const Preferences = new (class {
     }
 })(window);
 
+const FileDatabase = new (class {
+    ready (callback) {
+        this.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction  || window.msIDBTransaction;
+        window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+        var request = this.indexedDB.open('database', 1);
+
+        request.onsuccess = (e) => {
+            this.db = request.result;
+            callback();
+        }
+
+        request.onupgradeneeded = function (e) {
+            e.target.result.createObjectStore('files', { keyPath: 'timestamp' });
+        }
+    }
+
+    set (object) {
+        this.db.transaction(['files'], 'readwrite').objectStore('files').add(object);
+    }
+
+    get (callback) {
+        var result = [];
+        this.db.transaction('files').objectStore('files').openCursor().onsuccess = function (event) {
+            var cursor = event.target.result;
+            if (cursor) {
+                result.push(cursor.value);
+                cursor.continue();
+            } else {
+                callback(result);
+            }
+        }
+    }
+
+    remove (key) {
+        this.db.transaction(['files'], 'readwrite').objectStore('files').delete(key);
+    }
+})();
+
 const Handle = new (class {
     constructor () {
         this.listeners = {};
@@ -51,35 +91,46 @@ const Handle = new (class {
 })();
 
 const Database = new (class {
-    from (array) {
-        this.Players = {};
-        this.Groups = {};
+    remove (timestamp) {
+        Object.values(this.Players).forEach(function (player) {
+            if (player.List.length == 1 && player.List[0].timestamp == timestamp) {
+                delete Database.Players[player.Latest.Identifier];
+            } else {
+                delete player[timestamp];
+            }
+        });
 
-        array.sort((a, b) => a.timestamp - b.timestamp);
+        Object.values(this.Groups).forEach(function (group) {
+            if (group.List.length == 1 && group.List[0].timestamp == timestamp) {
+                delete Database.Groups[group.Latest.Identifier];
+            } else {
+                delete group[timestamp];
+            }
+        })
 
-        for (var file of array) {
-            for (const groupdata of file.groups) {
-                let group = new Group(groupdata);
+        this.update();
+    }
 
-                if (!this.Groups[group.Identifier]) {
-                    this.Groups[group.Identifier] = {
-                        List: []
-                    };
+    add (files) {
+        var tempGroups = {};
+        var tempPlayers = {};
+
+        for (var file of files) {
+            for (var data of file.groups) {
+                let group = new Group(data);
+
+                if (!tempGroups[group.Identifier]) {
+                    tempGroups[group.Identifier] = {};
                 }
-
-                this.Groups[group.Identifier][file.timestamp] = group;
-                this.Groups[group.Identifier].List.push({
-                    timestamp: file.timestamp,
-                    group: group
-                });
+                tempGroups[group.Identifier][file.timestamp] = group;
             }
 
-            for (const playerdata of file.players) {
-                let player = new Player(playerdata);
+            for (var data of file.players) {
+                let player = new Player(data);
 
-                let gid = player.Group ? Object.keys(this.Groups).find(g => this.Groups[g][file.timestamp] && this.Groups[g][file.timestamp].Name == player.Group.Name) : null;
-                if (gid) {
-                    let group = this.Groups[gid][file.timestamp];
+                let groupIdentifier = player.Group ? Object.keys(tempGroups).find(id => tempGroups[id] && tempGroups[id][file.timestamp] && tempGroups[id][file.timestamp].Name == player.Group.Name) : null;
+                if (groupIdentifier) {
+                    let group = tempGroups[groupIdentifier][file.timestamp];
                     let index = group.MemberIDs.findIndex(p => p === player.Identifier);
 
                     player.Group.Role = group.Roles[index];
@@ -93,67 +144,111 @@ const Database = new (class {
                     }
                 }
 
-                if (!this.Players[player.Identifier]) {
-                    this.Players[player.Identifier] = {
-                        List: []
-                    };
+                if (!tempPlayers[player.Identifier]) {
+                    tempPlayers[player.Identifier] = {};
                 }
-
-                this.Players[player.Identifier][file.timestamp] = player;
-                this.Players[player.Identifier].List.push({
-                    timestamp: file.timestamp,
-                    player: player
-                });
+                tempPlayers[player.Identifier][file.timestamp] = player;
             }
         }
 
-        for (const [pid, instances] of Object.entries(this.Players)) {
-            instances.LatestTimestamp = Math.max(... Object.keys(instances).filter(value => !isNaN(value)));
-            instances.Latest = instances[instances.LatestTimestamp];
+        Object.entries(tempPlayers).forEach((entry) => {
+            if (!this.Players[entry[0]]) {
+                this.Players[entry[0]] = {};
+            }
+
+            Object.entries(entry[1]).forEach((subentry) => {
+                this.Players[entry[0]][subentry[0]] = subentry[1];
+            });
+        });
+
+        Object.entries(tempGroups).forEach((entry) => {
+            if (!this.Groups[entry[0]]) {
+                this.Groups[entry[0]] = {};
+            }
+
+            Object.entries(entry[1]).forEach((subentry) => {
+                this.Groups[entry[0]][subentry[0]] = subentry[1];
+            });
+        });
+
+        this.update();
+    }
+
+    from (files) {
+        this.Players = {};
+        this.Groups = {};
+
+        this.add(files);
+    }
+
+    update () {
+        this.Latest = 0;
+
+        for (const [id, obj] of Object.entries(this.Players)) {
+            obj.List = Object.entries(obj).map(function (entry) {
+                return {
+                    timestamp: Number(entry[0]),
+                    player: entry[1]
+                };
+            }).filter(entry => !isNaN(entry.timestamp));
+
+            obj.List.sort((a, b) => a.timestamp - b.timestamp);
+            obj.LatestTimestamp = obj.List[obj.List.length - 1].timestamp;
+            obj.Latest = obj[obj.LatestTimestamp];
+            if (obj.LatestTimestamp > this.Latest) {
+                this.Latest = obj.LatestTimestamp;
+            }
         }
 
-        for (const [gid, instances] of Object.entries(this.Groups)) {
-            instances.LatestTimestamp = Math.max(... Object.keys(instances).filter(value => !isNaN(value)));
-            instances.Latest = instances[instances.LatestTimestamp];
-        }
+        for (const [id, obj] of Object.entries(this.Groups)) {
+            obj.List = Object.entries(obj).map(function (entry) {
+                return {
+                    timestamp: Number(entry[0]),
+                    group: entry[1]
+                };
+            }).filter(entry => !isNaN(entry.timestamp));
 
-        this.Latest = Math.max(... array.map(instance => instance.timestamp));
+            obj.List.sort((a, b) => a.timestamp - b.timestamp);
+            obj.LatestTimestamp = obj.List[obj.List.length - 1].timestamp;
+            obj.Latest = obj[obj.LatestTimestamp];
+            if (obj.LatestTimestamp > this.Latest) {
+                this.Latest = obj.LatestTimestamp;
+            }
+        }
     }
 })();
 
 const Storage = new (class {
-    constructor () {
-        this.load();
-    }
+    load (callback) {
+        FileDatabase.ready(() => {
+            FileDatabase.get((current) => {
+                this.current = current;
 
-    load () {
-        this.current = Preferences.get('data', [], true);
-        Database.from(this.current);
+                Database.from(this.current);
+                callback();
+            });
+        });
     }
 
     save () {
-        Preferences.set('data', this.current, true);
+        this.current.sort((a, b) => a.timestamp - b.timestamp);
+        this.current.forEach((f) => {
+            FileDatabase.set(f);
+        })
     }
 
     import (content) {
-        var files = null;
         try {
-            files = Object.values(JSON.parse(content)).filter(f => !this.current.includes(f.timestamp));
-        } catch (e) {
-            throw `File is not archive format.<br>(${ e })`;
-        }
+            var json = JSON.parse(content);
+            var list = json.filter(file => (this.current.find(cfile => cfile.timestamp == file.timestamp) == null));
 
-        try {
-            var copy = [... this.current, ... files];
-            Database.from(copy);
-        } catch (e) {
-            Database.from(this.current);
-            throw `Archive could not be imported.<br>${ e }`;
-        }
+            Database.add(list);
 
-        this.current.push(... files);
-        Database.from(this.current);
-        this.save();
+            this.current.push(... list);
+            this.save();
+        } catch (exception) {
+            throw exception.lineNumber + ' ' + exception;
+        }
     }
 
     export () {
@@ -165,114 +260,106 @@ const Storage = new (class {
     }
 
     add (content, timestamp) {
-        var json = null;
         try {
-            json = JSON.parse(content);
-        } catch (e) {
-            throw `File is not in JSON format.<br>(${ e })`;
-        }
-
-        var raws = [];
-        for (var [key, val, url] of filterPlayaJSON(json)) {
-            if (key === 'text' && (val.includes('otherplayername') || val.includes('othergroup') || val.includes('ownplayername'))) {
-                var url = url.split(/.*\/(.*)\.sfgame\.(.*)\/.*/g);
-                if (url.length > 2) {
-                    raws.push([val, url[1] + '_' + url[2]]);
+            var json = JSON.parse(content);
+            var raws = [];
+            for (var [key, val, url] of filterPlayaJSON(json)) {
+                if (key === 'text' && (val.includes('otherplayername') || val.includes('othergroup') || val.includes('ownplayername'))) {
+                    var url = url.split(/.*\/(.*)\.sfgame\.(.*)\/.*/g);
+                    if (url.length > 2) {
+                        raws.push([val, url[1] + '_' + url[2]]);
+                    }
                 }
             }
-        }
 
-        var file = {
-            groups: [],
-            players: [],
-            timestamp: timestamp
-        }
+            var file = {
+                groups: [],
+                players: [],
+                timestamp: timestamp
+            }
 
-        for (var pair of raws) {
-            var [raw, prefix] = pair;
+            for (var pair of raws) {
+                var [raw, prefix] = pair;
 
-            if (raw.includes('groupSave') || raw.includes('groupsave')) {
-                var group = {
-                    prefix: prefix
-                };
+                if (raw.includes('groupSave') || raw.includes('groupsave')) {
+                    var group = {
+                        prefix: prefix
+                    };
 
-                for (var [key, val] of parsePlayaResponse(raw)) {
-                    if (key.includes('groupname')) {
-                        group.name = val;
-                    } else if (key.includes('grouprank')) {
-                        group.rank = Number(val);
-                    } else if (key.includes('groupmember')) {
-                        group.members = val.split(',');
-                    } else if (key.includes('groupknights')) {
-                        group.knights = val.split(',').map(a => Number(a));
-                    } else if (key.includes('owngroupsave')) {
-                        group.save = val.split('/').map(a => Number(a));
-                        group.own = true;
-                    } else if (key.includes('groupSave')) {
-                        group.save = val.split('/').map(a => Number(a));
+                    for (var [key, val] of parsePlayaResponse(raw)) {
+                        if (key.includes('groupname')) {
+                            group.name = val;
+                        } else if (key.includes('grouprank')) {
+                            group.rank = Number(val);
+                        } else if (key.includes('groupmember')) {
+                            group.members = val.split(',');
+                        } else if (key.includes('groupknights')) {
+                            group.knights = val.split(',').map(a => Number(a));
+                        } else if (key.includes('owngroupsave')) {
+                            group.save = val.split('/').map(a => Number(a));
+                            group.own = true;
+                        } else if (key.includes('groupSave')) {
+                            group.save = val.split('/').map(a => Number(a));
+                        }
+                    }
+
+                    if (!file.groups.find(g => g.name === group.name) && group.members) {
+                        file.groups.push(group);
                     }
                 }
 
-                if (!file.groups.find(g => g.name === group.name) && group.members) {
-                    file.groups.push(group);
-                }
-            }
+                if (raw.includes('otherplayername') || raw.includes('ownplayername')) {
+                    let player = {
+                        prefix: prefix
+                    };
 
-            if (raw.includes('otherplayername') || raw.includes('ownplayername')) {
-                let player = {
-                    prefix: prefix
-                };
+                    for (var [key, val] of parsePlayaResponse(raw)) {
+                        if (key.includes('groupname')) {
+                            player.groupname = val;
+                        } else if (key.includes('name')) {
+                            player.name = val;
+                        } else if (key.includes('unitlevel')) {
+                            player.units = val.split('/').map(a => Number(a));
+                        } else if (key.includes('achievement') && !key.includes('new')) {
+                            player.achievements = val.split('/').map(a => Number(a));
+                        } else if (key.includes('fortressrank')) {
+                            player.fortressrank = Number(val);
+                        } else if (key.includes('playerlookat')) {
+                            player.save = val.split('/').map(a => Number(a));
+                        } else if (key.includes('playerSave')) {
+                            player.save = val.split('/').map(a => Number(a));
+                            player.own = true;
+                        }
+                    }
 
-                for (var [key, val] of parsePlayaResponse(raw)) {
-                    if (key.includes('groupname')) {
-                        player.groupname = val;
-                    } else if (key.includes('name')) {
-                        player.name = val;
-                    } else if (key.includes('unitlevel')) {
-                        player.units = val.split('/').map(a => Number(a));
-                    } else if (key.includes('achievement') && !key.includes('new')) {
-                        player.achievements = val.split('/').map(a => Number(a));
-                    } else if (key.includes('fortressrank')) {
-                        player.fortressrank = Number(val);
-                    } else if (key.includes('playerlookat')) {
-                        player.save = val.split('/').map(a => Number(a));
-                    } else if (key.includes('playerSave')) {
-                        player.save = val.split('/').map(a => Number(a));
-                        player.own = true;
+                    if (!file.players.find(p => p.name === player.name)) {
+                        file.players.push(player);
                     }
                 }
-
-                if (!file.players.find(p => p.name === player.name)) {
-                    file.players.push(player);
-                }
             }
-        }
 
-        if (this.current.find(x => x.timestamp == file.timestamp)) {
-            return;
-        }
+            if (this.current.find(x => x.timestamp == file.timestamp)) {
+                return;
+            }
 
-        if (file.players.length == 0 || file.groups.length == 0) {
-            throw 'The file must contain at least one group and one player.';
-        }
+            if (file.players.length == 0 || file.groups.length == 0) {
+                throw 'The file must contain at least one group and one player.';
+            }
 
-        try {
-            var copy = [... this.current, file];
-            Database.from(copy);
-        } catch (e) {
-            Database.from(this.current);
-            throw `Unexpected error appeared while database was generated.<br>${ e }`;
-        }
+            Database.add([ file ]);
 
-        this.current.push(file);
-        Database.from(this.current);
-        this.save();
+            this.current.push(file);
+            this.save();
+        } catch (exception) {
+            throw exception.lineNumber + ' ' + exception;
+        }
     }
 
     remove (index) {
+        FileDatabase.remove(this.current[index].timestamp);
+        Database.remove(this.current[index].timestamp);
+
         this.current.splice(index, 1);
-        Database.from(this.current);
-        this.save();
     }
 
     files () {
