107873bc27453d7cd830989f9eb0c47ef3d8f77f
Martin Halfar <HafisCZ@users.noreply.github.com>
2020-01-23 00:56:10 +0100

    Preparations for Arena Manager

---
+++ js/idle.js
@@ -0,0 +1,177 @@
+const InitialCosts = [
+    5,
+    100,
+    2500,
+    50000,
+    1000000,
+    25000000,
+    500000000,
+    10000000000,
+    250000000000,
+    5000000000000
+];
+
+const Multipliers = [
+    1, 10, 25, 100, 1000
+];
+
+const MultiplierBinding = {
+    1: 1,
+    10: 2,
+    25: 3,
+    100: 4,
+    1000: 5
+};
+
+const PriceTable = (function (max, costs, multipliers) {
+    var table = [];
+    for (var i = 0; i < costs.length; i++) {
+        table[i] = [];
+        for (var j = 0; j < multipliers.length; j++) {
+            table[i][j] = [];
+        }
+        table[i][0][0] = costs[i];
+        for (var j = 1; j < max; j++) {
+            table[i][0][j] = Math.ceil(1.03 * table[i][0][j - 1]);
+        }
+        for (var j = 1; j < multipliers.length; j++) {
+            for (var k = 0; k < (max - multipliers[j]); k++) {
+                var cost = 0;
+                for (var l = 0; l < multipliers[j]; l++) {
+                    cost += table[i][0][k + l];
+                }
+                table[i][j][k] = cost;
+            }
+        }
+    }
+    return table;
+})(10000, InitialCosts, Multipliers);
+
+function getRunesFromMoney (money) {
+    return Math.trunc((Math.PI * 11592991 / 10748438389408) * Math.pow(money, 0.83));
+}
+
+class Building {
+    constructor (id, initialDuration, initialIncrement, initialCost, initialLevel) {
+        this.id = id;
+        this.initialDuration = initialDuration;
+        this.initialIncrement = initialIncrement;
+        this.initialCost = initialCost;
+        this.initialLevel = initialLevel;
+    }
+
+    getBaseLevel () {
+        return this.initialLevel;
+    }
+
+    getUpgradePrice (level, bulk) {
+        var amount = MultiplierBinding[bulk];
+        if (amount) {
+            return PriceTable[this.id][amount - 1][level];
+        } else {
+            var price = 0;
+            for (var i = 0; i < Multipliers.length; i++) {
+                while (bulk - level > Multipliers[i]) {
+                    price += PriceTable[this.id][i][level];
+                    level += Multipliers[i];
+                }
+            }
+            return price;
+        }
+    }
+
+    getCycleDuration (level) {
+        return this.initialDuration * Math.pow(0.8, Building.getNearestBreakpoint(level));
+    }
+
+    getCycleProduction (level) {
+        return Math.round(this.initialIncrement * level * Math.pow(2, Building.getNearestBreakpoint(level)));
+    }
+
+    getCycleIncrement (level) {
+        return this.initialIncrement * Math.pow(2, Building.getNearestBreakpoint(level));
+    }
+
+    getAmortisation (level) {
+        return this.getUpgradePrice(level - 1) / ((this.initialIncrement * Math.pow(2, Building.getNearestBreakpoint(level))) / (this.initialDuration * Math.pow(0.8, Building.getNearestBreakpoint(level))));
+    }
+
+    getBreakpointAmortisation (level) {
+        var nb = Building.getBreakpointLevel(Building.getNearestBreakpoint(level) + 1);
+        return this.getUpgradePrice(level, nb - level) / ((this.getCycleProduction(nb) - this.getCycleProduction(level)) / this.getCycleDuration(nb));
+    }
+
+    getProductionRate (level) {
+        return this.getCycleProduction(level) / this.getCycleDuration(level);
+    }
+
+    matchProductionRate (level, matched) {
+        return this.matchProductionRateBetween(matched, this.getProductionRate(Math.max(1, level)), 0, 10000);
+    }
+
+    matchProductionRateBetween (building, minimalRate, min, max) {
+        if (max - min <= 2) {
+            if (building.getProductionRate(min) >= minimalRate) {
+                return min;
+            } else if (building.getProductionRate(min + 1) < minimalRate) {
+                return max;
+            } else {
+                return min + 1;
+            }
+        } else {
+            let mid = Math.ceil((max + min) / 2);
+            if (building.getProductionRate(mid) > minimalRate) {
+                return this.matchProductionRateBetween(building, minimalRate, min, mid);
+            } else {
+                return this.matchProductionRateBetween(building, minimalRate, mid, max);
+            }
+        }
+    }
+
+    static getNearestBreakpoint (level) {
+        if (level < 25) {
+            return 0;
+        } else if (level < 50) {
+            return 1;
+        } else if (level < 100) {
+            return 2;
+        } else if (level < 250) {
+            return 3;
+        } else if (level < 500) {
+            return 4;
+        } else if (level < 1000) {
+            return 5;
+        } else if (level < 2500) {
+            return 6;
+        } else if (level < 5000) {
+            return 7;
+        } else {
+            return 8;
+        }
+    }
+
+    static getBreakpointLevel (breakpoint) {
+        if (breakpoint > 3) {
+            return 10 * Building.getBreakpointLevel(breakpoint - 3);
+        } else if (breakpoint == 3) {
+            return 100;
+        } else if (breakpoint == 2) {
+            return 50;
+        } else if (breakpoint == 1) {
+            return 25;
+        } else {
+            return 0;
+        }
+    }
+}
+
+Building.Seat = new Building(0, 72, 1, 5, 1);
+Building.PopcornStand = new Building(1, 360, 10, 100, 0);
+Building.ParkingLot = new Building(2, 720, 40, 2500, 0);
+Building.Trap = new Building(3, 1080, 120, 50000, 0);
+Building.Drinks = new Building(4, 1440, 320, 1000000, 0);
+Building.DeadlyTrap = new Building(5, 2160, 960, 25000000, 0);
+Building.VIPSeat = new Building(6, 2880, 2560, 500000000, 0);
+Building.Snacks = new Building(7, 4320, 7680, 10000000000, 0);
+Building.StrayingMonsters = new Building(8, 8640, 30720, 250000000000, 0);
+Building.Toilet = new Building(9, 21600, 153600, 5000000000000, 0);
