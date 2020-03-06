a23263c8981bded7c55024b09e51513c3ecea65b
Martin Halfar <HafisCZ@users.noreply.github.com>
2020-03-06 22:56:59 +0100

    Fixed time formatting

--- js/util.js
+++ js/util.js
@@ -5,19 +5,20 @@ function trail (c, n) {
 
 // Format date
 function formatDateOnly (date) {
-    if (date == '') return '';
+    if (date == '' || date == undefined || date <= 0) return '';
     date = new Date(date);
     return trail(date.getDate(), 2) + '.' + trail(date.getMonth() + 1, 2) + '.' + date.getFullYear();
 }
 
 // Format datetime
 function formatDate (date) {
-    if (date == '') return '';
+    if (date == '' || date == undefined || date <= 0) return '';
     date = new Date(date);
     return trail(date.getDate(), 2) + '.' + trail(date.getMonth() + 1, 2) + '.' + date.getFullYear() + ' ' + trail(date.getHours(), 2) + ':' + trail(date.getMinutes(), 2);
 }
 
 function formatDuration (duration) {
+    if (duration == '' || duration == undefined || duration <= 0) return '';
     var days = Math.trunc(duration / (1000 * 60 * 60 * 24));
     var hours = Math.trunc((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.trunc((duration % (1000 * 60 * 60)) / (1000 * 60));
