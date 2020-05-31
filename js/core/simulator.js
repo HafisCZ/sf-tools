2898ed943bd394d038ee948166d78f84fb4e0ba7
Martin Halfar <HafisCZ@users.noreply.github.com>
2020-05-31 13:16:43 +0200

    Fixed critical multiplier

--- js/core/simulator.js
+++ js/core/simulator.js
@@ -926,7 +926,6 @@ class PetModel {
         this.Attribute = Math.trunc(PET_FACTOR_MAP[this.Pet - 1] * (this.Level + 1) * (1 + this.Bonus / 100));
         this.DefenseAttribute = Math.trunc(this.Attribute / 2);
         this.TotalHealth = (this.Level + 1) * this.Attribute * (this.Class == WARRIOR ? 5 : (this.Class == MAGE ? 2 : 4));
-        this.Critical = 2 * (1 + 5 * this.Gladiator / 100);
     }
 
     initialize (target) {
@@ -934,6 +933,7 @@ class PetModel {
         var multb = (1 + Math.max(this.Attribute / 2, this.Attribute - target.getDefenseAtribute(this)) / 10);
         this.Damage = Math.trunc(multa * multb);
 
+        this.Critical = 2 * (1 + 0.05 * Math.max(0, this.Gladiator - target.Gladiator));
         this.SkipChance = target.Class == MAGE ? 0 : (this.Class == WARRIOR ? 25 : (this.Class == MAGE ? 0 : 50));
         this.CriticalChance = Math.min(50, this.Attribute * 20 / 6 / target.Level);
     }
