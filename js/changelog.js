5098677b17e92f553254500242819305321c5c01
Martin Halfar <HafisCZ@users.noreply.github.com>
2020-07-08 23:04:06 +0200

    Changed changelog wording

--- js/changelog.js
+++ js/changelog.js
@@ -1,6 +1,6 @@
 const CHANGELOG = {
     'v3.616': [
-        '<code>Last Active</code> header no longer works as before.<br/>To use colors like before, you\'ll need to use default template or modify the header in this way:<br/><br/><img src="res/update0.png" style="margin-left: -2em;"></img>'
+        '<code>Last Active</code> header no longer works as before.<br/>To use colors like before, you\'ll need to modify the header in this way:<br/><br/><img src="res/update0.png" style="margin-left: -2em;"></img>'
     ],
     'v3.610': [
         'Migrated <code>Album</code>, <code>Mount</code> and few other headers over to the new header system.',
