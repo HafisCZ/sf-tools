e2dc0ab35fe73df1baae9a7d1ac839d72ce26b2d
Martin Halfar <HafisCZ@users.noreply.github.com>
2020-08-16 16:23:47 +0200

    Fixed incorrect scope

--- js/core/ast.js
+++ js/core/ast.js
@@ -1460,8 +1460,8 @@ class AST2 {
 
                     return mapper.ast.eval(player, reference, environment, scope2, extra);
                 } else if (node.op == 'difference' && node.args.length == 1) {
-                    var a = this.evalInternal(player, reference, environment, scope, extra, node.args[0]);
-                    var b = this.evalInternal(reference, reference, environment, scope, extra, node.args[0]);
+                    var a = this.evalInternal(player, reference, environment, player, extra, node.args[0]);
+                    var b = this.evalInternal(reference, reference, environment, reference, extra, node.args[0]);
 
                     if (isNaN(a) || isNaN(b)) {
                         return undefined;
