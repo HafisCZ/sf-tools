e6b7b740f6abfb3c415d4e7f5b60e32bd9fa64eb
Martin Halfar <HafisCZ@users.noreply.github.com>
2021-02-05 21:59:57 +0100

    Added sub 10% hp chance

--- js/sim/dungeons.js
+++ js/sim/dungeons.js
@@ -448,7 +448,8 @@ class DungeonSimulator {
             winrate: 100 * score / iterations,
             health_min: 100 * Math.min(... healths),
             health_max: 100 * Math.max(... healths),
-            health_avg: 100 * healths.reduce((a, b) => a + b, 0) / healths.length
+            health_avg: 100 * healths.reduce((a, b) => a + b, 0) / healths.length,
+            health_sub10: 100 * healths.reduce((a, b) => a + (b < 0.1 ? 1 : 0), 0) / healths.length
         };
     }
 
