6096a0c026c42498bd8979d732ca29b35a5b2011
Martin Halfar <HafisCZ@users.noreply.github.com>
2021-02-05 21:45:48 +0100

    chore: updated dungeon data

--- js/sim/dungeon_data.js
+++ js/sim/dungeon_data.js
@@ -3,22 +3,39 @@ DUNGEON_DATA = {
         name: 'Desecrated Catacombs',
         shadow: false,
         floors: {
-            'Ghost': { pos: 1, id: 0, class: 2, level: 10, str: 48, dex: 63, int: 104, con: 77, lck: 47, health: 1694, runes: null, min: 33, max: 40 }
+            'Ghost': { pos: 1, id: 0, class: 2, level: 10, str: 48, dex: 63, int: 104, con: 77, lck: 47, health: 1694, min: 33, max: 40 }
         }
     },
     '12': {
         name: 'Hell',
         shadow: false,
         floors: {
-            'Restless Soul': { pos: 1, id: 0, class: 2, level: 305, str: 1475, dex: 1495, int: 6020, con: 5580, lck: 2145, health: 3414960, runes: null, min: 669, max: 1150 },
-            'Furious Soul': { pos: 2, id: 0, class: 2, level: 310, str: 1500, dex: 1520, int: 6120, con: 5680, lck: 2180, health: 3532960, runes: null, min: 679, max: 1185 }
+            'Restless Soul': { pos: 1, id: 0, class: 2, level: 305, str: 1475, dex: 1495, int: 6020, con: 5580, lck: 2145, health: 3414960, min: 669, max: 1150 },
+            'Furious Soul': { pos: 2, id: 0, class: 2, level: 310, str: 1500, dex: 1520, int: 6120, con: 5680, lck: 2180, health: 3532960, min: 679, max: 1185 }
         }
     },
     '13': {
         name: '13th Floor',
         shadow: false,
         floors: {
-            'Hellgore the Hellish': { pos: 1, id: 0, class: 1, level: 355, str: 7570, dex: 2655, int: 2630, con: 7290, lck: 1716, health: 12976200, runes: null, min: 401, max: 733 }
+            'Hellgore the Hellish': { pos: 1, id: 0, class: 1, level: 355, str: 7570, dex: 2655, int: 2630, con: 7290, lck: 1716, health: 12976200, min: 401, max: 733 }
+        }
+    },
+    '14': {
+        name: 'Easteros',
+        shadow: false,
+        floors: {
+            'Robert Drunkatheon': { pos: 1, id: 0, class: 1, level: 310, str: 6040, dex: 2120, int: 2095, con: 5760, lck: 1410, health: 8956800, min: 325, max: 504 },
+            'Lefty Lennister': { pos: 2, id: 0, class: 1, level: 320, str: 6240, dex: 2190, int: 2165, con: 5960, lck: 1450, health: 9565800, min: 335, max: 534 },
+            'Petyr the Pimp': { pos: 3, id: 0, class: 2, level: 330, str: 3200, dex: 3240, int: 13040, con: 12160, lck: 4640, health: 8049920, min: 719, max: 1325 },
+            'Holundor': { pos: 4, id: 0, class: 1, level: 340, str: 13280, dex: 4660, int: 4610, con: 12720, lck: 3060, health: 21687600, min: 355, max: 594 }
+        }
+    },
+    '20': {
+        name: 'Mount Olympus',
+        shadow: false,
+        floors: {
+            'Hades': { pos: 9, id: 0, class: 2, level: 455, str: 28000, dex: 40000, int: 28000, con: 250000, lck: 13000, health: 697500032, runes: { type: 40, resist: 60, damage: 60 }, min: 761, max: 1518 }
         }
     }
 };
