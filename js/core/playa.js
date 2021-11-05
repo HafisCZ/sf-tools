e106aec2b2e7a0ffbd386b06f397ee761cd5446b
Martin <HafisCZ@users.noreply.github.com>
2021-11-05 15:57:25 +0100

    Removed guild member count calculation

--- js/core/playa.js
+++ js/core/playa.js
@@ -533,7 +533,7 @@ class SFGroup {
         this.Timestamp = data.timestamp;
 
         this.MembersPresent = 0;
-        this.MemberCount = data.save[3];
+        // this.MemberCount = data.save[3];
         this.Honor = data.save[13];
         this.Pet = data.save[378];
 
@@ -570,9 +570,6 @@ class SFGroup {
                     this.Knights.splice(i, 1);
                 }
 
-                if (this.Roles[i] == GUILD_ROLE_INVITED) {
-                    this.MemberCount--;
-                }
                 this.Roles.splice(i, 1);
                 this.Treasures.splice(i, 1);
                 this.Instructors.splice(i, 1);
@@ -584,6 +581,7 @@ class SFGroup {
             }
         }
 
+        this.MemberCount = this.Members.length;
         this.TotalKnights = data.save[370];
         this.TotalInstructor = Math.trunc(Math.min(_sum(this.Instructors), 500) / 5);
         this.TotalTreasure = Math.trunc(Math.min(_sum(this.Treasures), 500) / 5);
