96f38a2be3789f46bcaae9c296c5c203c2f3a602
Martin <HafisCZ@users.noreply.github.com>
2021-11-30 22:22:36 +0000

    Removed debug log output

--- js/util.js
+++ js/util.js
@@ -401,7 +401,7 @@ function getColorFromHSLA (h, s, l, a) {
         g = hue2rgb(p, q, h);
         b = hue2rgb(p, q, h - 1 / 3);
     }
-console.log(r, g, b);
+
     return getColorFromRGBA(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), a);
 }
 
