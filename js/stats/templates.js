ea3fc5b13658c947cd526aca036b713d094f2c75
Martin <HafisCZ@users.noreply.github.com>
2022-01-02 15:48:43 +0000

    Added new default players template

--- js/stats/templates.js
+++ js/stats/templates.js
@@ -56,135 +56,125 @@ color default @red
 category Fortress
 
 header Fortress Honor`,
-    'Players Default': `# Created by Acclamator
+    'Players Default': `# Color scheme
+const green d3e4cd
+const orange ffd3b5
+const red DF6A6A
+const purple bfa2db
+const yellow ffffc1
+const blue b0deff
+const air e1f2fb
+const pink ffcfdf
+const navy 424874
+const darkgreen 889e81
+const beige E2C2B9
 
-server 70 # Set server column width to 70px
-name @normal # Set name column width to normal
-indexed static # Show static index column on left side
+background @air
+font 95%
+name 200
 
+category
 
-category General
+  header Class # Compact class header
+  alias @empty
+  width @tiny
+  format this == @druid ? [ 'D', 'BD', 'CD' ][Mask] : [ '', 'W', 'M', 'S', 'A', 'BM', 'B', 'DH' ][this]
+  expc this == @druid ? dualcolor(50, [ @navy, @blue, @darkgreen ][Mask], @navy) : [ '', @blue, @green, @darkgreen, @yellow, @pink, @orange, @purple, @navy ][this]
+  border right
 
-header UP # This column indicates how recent the player information is
-expr now() - Timestamp # Calculate how long ago was the player captured
-width 40 # Set column width to 40 pixels
-flip on # Treat lower values as better
-color above @21days darkred # Color the cell in dark red if the value is above 21 days
-color above @7days red
-color above @3days darkorange
-color above @1day orange
-color above @12hours yellow
-color default @green
-value default @empty # Column has no values
+  header Level
+  expc gradient({ 0: @red, 100: @orange, 200: @yellow, 300: @green, 400: @pink, 500: @purple, 600: @blue, 700: @navy }, this)
+  difference on
+  width 100
 
-header Class
-width @small
-value equal 1 W # Show W if the cell has value of 1
-value equal 2 M
-value equal 3 S
-value equal 4 A
-value equal 5 BM
-value equal 6 B
-value equal 7 DH
-value equal 8 D
-color equal 1 1874CD
-color equal 2 darkorchid
-color equal 3 lawngreen
-color equal 4 red
-color equal 5 violet
-color equal 6 orange
-color equal 7 eb605e
-color equal 8 13d159
+  header Album
+  # Adjustable album min & ok values
+  const album_warning 0.75
+  const album_ok 0.85
+  expc gradient({ 0: @red, @album_warning: @orange, @album_ok: @yellow, 1: @green }, this / 100)
+  expf fixed(this, 2) + ' %'
+  width 100
 
-header Gear # This column indicates what gear has the player equipped
-expr (Rune Gold + Rune XP >= 50 ? "Q" : "F") # Add gold and xp runes and decide whether gear is for quests or not
-width @small
-color equal Q lightblue
-color equal F orange
+  header Mount
+  color equal @mount50 @green
+  color equal @mount30 @yellow
+  color equal @mount20 @orange
+  color default @red
 
-header Level
-width @small
+category Basis
+  width 100
 
-header Awards
-width @small
-hydra on # Show hydra
+  header Base
+  alias Main
+  format fnumber
 
-
-category Base
-
-header Base Sum
-expr Base + Base Constitution # Calculate total basis for primary attribute and constitution
-
-header Ratio
-expr ceil(Base / Base Constitution * 10) / 10 # Calculate the ratio between primary basis and constitution basis
-format this + ':1' # Add :1 to the ratio
-
-header Base
-alias Main # Rename header to Main
-header Base Constitution
-alias Con
-
+  header Base Constitution
+  alias Con
+  format fnumber
 
 category Total
+  width 100
 
-header Total Sum
-expr Attribute + Constitution
+  header Attribute
+  alias Main
+  format fnumber
 
-header Ratio
-expr ceil(Attribute / Constitution * 10) / 10
-format this + ':1'
+  header Constitution
+  alias Con
+  format fnumber
 
-header Attribute
-alias Main
-header Constitution
-alias Con
+category
 
+  header Ratio
+  expr trunc(100 * Attribute / (Attribute + Constitution))
+  expf format('{0}/{1}', this, 100 - this)
+  order by -max(0, abs(this - 55))
 
 category Fortress
 
-header Gem Mine
-width @small
+  header Gem Mine
+  alias Mine
+  width 80
 
-header Treasury
-width @small
+  header Treasury
+  width 80
 
-header Raid
-expr Raid Wood + Raid Stone
-color above 150000000 green
-color above 100000000 yellow
-color above 50000000 orange
-color above 10000000 darkorange
-color default red
+  header Fortress Honor
+  alias Honor
+  format fnumber
+  width 100
 
+category Guild
+  difference on
 
-category Activity
+  not defined value @empty
+  not defined color @air
 
-header Status # This column indicates what the player is doing right now
-alias ATM
-width 30
-color equal 0 @red
-color equal 1 @orange
-color equal 2 @green
-value equal 0 I
-value equal 1 W
-value equal 2 Q
+  header Pet
+  expc dualcolor(10, Hydra Dead ? @air: @blue, @air)
+  width 80
 
-header Mount
-color equal 4 @green
-color equal 3 @orange
-color equal 2  darkorange
-color equal 1  darkorange
-color default @red
+  header Knights
+  alias HOK
+  width 80
+  # Adjustable knights min & ok values
+  const knights_warning 15
+  const knights_ok 18
+  expc gradient({ 0: @red, @knights_warning: @orange, @knights_ok: @yellow, 20: @green }, this)
+  difference on
 
-header Inactive Time # This column indicated how long ago was the player active (referenced against capture date)
-alias Last Active
-format datetime(Last Active)
-color above @21days darkred
-color above @7days red
-color above @3days darkorange
-color above @1day orange
-color above @12hours yellow
-color default @green`,
+  header Last Active
+  width 160
+  difference off
+  expr Last Active < 0 ? undefined : Inactive Time
+  expf datetime(Last Active)
+  order by Last Active < 0 ? undefined : -(Inactive Time)
+  color above @21days @red
+  color above @7days @beige
+  color above @3days @orange
+  color above @1day @yellow
+  color above @12hours @green
+  color default @blue`,
     'Guilds Default': `# Created by Acclamator
 
 # Show member list
