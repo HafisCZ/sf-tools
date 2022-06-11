fa7afe24fb5fa34e8d62cb9eac7cca35c369a0e2
Martin <HafisCZ@users.noreply.github.com>
2022-06-11 20:26:27 +0100

    Share more code

--- js/sim/players.js
+++ js/sim/players.js
@@ -1,3 +1,6 @@
+FIGHT_DUMP_ENABLED = false;
+FIGHT_DUMP_OUTPUT = [];
+
 const SimulatorType = {
     Pet: 1,
     PetMap: 2,
@@ -8,9 +11,6 @@ const SimulatorType = {
     Dungeon: 7,
     PetPath: 8
 }
-
-FIGHT_DUMP_ENABLED = false;
-FIGHT_DUMP_OUTPUT = [];
 
 // WebWorker hooks
 self.addEventListener('message', function (message) {
@@ -65,7 +65,7 @@ self.addEventListener('message', function (message) {
     self.close();
 });
 
-class GuildSimulator {
+class GuildSimulator extends SimulatorBase {
     simulate (guildA, guildB, iterations = 10000) {
         var score = 0;
 
@@ -147,30 +147,6 @@ class GuildSimulator {
         return (this.la.length > 0 ? this.la[0].Index : this.lb[0].Index) == 0;
     }
 
-    setRandomInitialFighter () {
-        if (this.a.AttackFirst == this.b.AttackFirst ? getRandom(50) : this.b.AttackFirst) {
-            [this.a, this.b] = [this.b, this.a];
-        }
-    }
-
-    forwardToBersekerAttack () {
-        // Thanks to rafa97sam for testing and coding this part that broke me
-        if (this.b.Player.Class == BERSERKER && getRandom(50)) {
-            let turnIncrease = 1;
-
-            if (this.a.Player.Class == BERSERKER) {
-                while (getRandom(50)) {
-                    turnIncrease += 1;
-                    [this.a, this.b] = [this.b, this.a];
-                }
-            }
-
-            this.turn += turnIncrease;
-
-            [this.a, this.b] = [this.b, this.a];
-        }
-    }
-
     // Fighter battle
     fight () {
         // Turn counter
@@ -236,47 +212,9 @@ class GuildSimulator {
         // Winner
         return (this.a.Health > 0 ? this.a.Index : this.b.Index) == 0;
     }
-
-    skipAndAttack () {
-        this.turn++;
-
-        var damage3 = this.attack(this.a, this.b);
-        if (this.a.DamageDealt) {
-            this.a.onDamageDealt(this.b, damage3);
-        }
-
-        if (this.b.DamageTaken) {
-            return this.b.onDamageTaken(this.a, damage3) > 0;
-        } else {
-            this.b.Health -= damage3;
-            return this.b.Health >= 0
-        }
-    }
-
-    attack (source, target, weapon = source.Weapon1) {
-        var turn = this.turn++;
-        var rage = 1 + turn / 6;
-
-        var damage = 0;
-        var skipped = getRandom(target.SkipChance);
-        var critical = false;
-
-        if (!skipped) {
-            damage = rage * (Math.random() * (1 + weapon.Max - weapon.Min) + weapon.Min);
-
-            critical = getRandom(source.CriticalChance);
-            if (critical) {
-                damage *= source.Critical;
-            }
-
-            damage = Math.ceil(damage);
-        }
-
-        return damage;
-    }
 }
 
-class FightSimulator {
+class FightSimulator extends SimulatorBase {
     // Fight group
     simulate (players, iterations = 100000, target = null, assource = false) {
         var scores = [];
@@ -452,30 +390,6 @@ class FightSimulator {
         this.bs = this.cb.onFightStart(this.ca);
     }
 
-    setRandomInitialFighter () {
-        if (this.a.AttackFirst == this.b.AttackFirst ? getRandom(50) : this.b.AttackFirst) {
-            [this.a, this.b] = [this.b, this.a];
-        }
-    }
-
-    forwardToBersekerAttack () {
-        // Thanks to rafa97sam for testing and coding this part that broke me
-        if (this.b.Player.Class == BERSERKER && getRandom(50)) {
-            let turnIncrease = 1;
-
-            if (this.a.Player.Class == BERSERKER) {
-                while (getRandom(50)) {
-                    turnIncrease += 1;
-                    [this.a, this.b] = [this.b, this.a];
-                }
-            }
-
-            this.turn += turnIncrease;
-
-            [this.a, this.b] = [this.b, this.a];
-        }
-    }
-
     // Fight
     fight () {
         // Create fighters
@@ -563,149 +477,5 @@ class FightSimulator {
 
         // Winner
         return (this.a.Health > 0 ? this.a.Index : this.b.Index) == 0;
-    }
-
-    skipAndAttack () {
-        this.turn++;
-
-        var damage3 = this.attack(this.a, this.b, this.a.Weapon1, 2);
-        if (this.a.DamageDealt) {
-            this.a.onDamageDealt(this.b, damage3);
-        }
-
-        if (this.b.DamageTaken) {
-            var alive = this.b.onDamageTaken(this.a, damage3);
-
-            if (FIGHT_DUMP_ENABLED && alive == 2) this.log(5);
-
-            return alive > 0;
-        } else {
-            this.b.Health -= damage3;
-            return this.b.Health >= 0
-        }
-    }
-
-    // Attack
-    attack (source, target, weapon = source.Weapon1, extra = 0) {
-        // Rage
-        var turn = this.turn++;
-        var rage = 1 + turn / 6;
-
-        // Test for skip
-        var damage = 0;
-        var skipped = getRandom(target.SkipChance);
-        var critical = false;
-
-        if (!skipped) {
-            damage = rage * (Math.random() * (weapon.Max - weapon.Min) + weapon.Min);
-
-            critical = getRandom(source.CriticalChance);
-            if (critical) {
-                damage *= source.Critical;
-            }
-
-            damage = Math.ceil(damage);
-        }
-
-        if (FIGHT_DUMP_ENABLED) this.log(4, source, target, weapon, damage, skipped, critical, extra);
-
-        return damage;
-    }
-
-    log (stage, ... args) {
-        if (stage == 0) {
-            this.log_obj = {
-                targetA: {
-                    ID: this.a.Player.ID || this.a.Index,
-                    Name: this.a.Player.Name,
-                    Level: this.a.Player.Level,
-                    MaximumLife: this.a.TotalHealth,
-                    Life: this.a.TotalHealth,
-                    Strength: this.a.Player.Strength.Total,
-                    Dexterity: this.a.Player.Dexterity.Total,
-                    Intelligence: this.a.Player.Intelligence.Total,
-                    Constitution: this.a.Player.Constitution.Total,
-                    Luck: this.a.Player.Luck.Total,
-                    Face: this.a.Player.Face,
-                    Race: this.a.Player.Race,
-                    Gender: this.a.Player.Gender,
-                    Class: this.a.Player.Class,
-                    Wpn1: this.a.Player.Items.Wpn1,
-                    Wpn2: this.a.Player.Items.Wpn2
-                },
-                targetB: {
-                    ID: this.b.Player.ID || this.b.Index,
-                    Name: this.b.Player.Name,
-                    Level: this.b.Player.Level,
-                    MaximumLife: this.b.TotalHealth,
-                    Life: this.b.TotalHealth,
-                    Strength: this.b.Player.Strength.Total,
-                    Dexterity: this.b.Player.Dexterity.Total,
-                    Intelligence: this.b.Player.Intelligence.Total,
-                    Constitution: this.b.Player.Constitution.Total,
-                    Luck: this.b.Player.Luck.Total,
-                    Face: this.b.Player.Face,
-                    Race: this.b.Player.Race,
-                    Gender: this.b.Player.Gender,
-                    Class: this.b.Player.Class,
-                    Wpn1: this.b.Player.Items.Wpn1,
-                    Wpn2: this.b.Player.Items.Wpn2
-                },
-                rounds: []
-            };
-
-            FIGHT_DUMP_OUTPUT.push(this.log_obj);
-        } else if (stage == 1) {
-            this.log_obj.rounds.push({
-                attackCrit: false,
-                attackType: 15,
-                attackMissed: false,
-                attackDamage: this.as,
-                attackSecondary: false,
-                attacker: this.a.Player.ID || this.a.Index,
-                target: this.b.Player.ID || this.b.Index
-            });
-        } else if (stage == 2) {
-            this.log_obj.rounds.push({
-                attackCrit: false,
-                attackType: 15,
-                attackMissed: false,
-                attackSecondary: false,
-                attackDamage: this.bs,
-                attacker: this.b.Player.ID || this.b.Index,
-                target: this.a.Player.ID || this.a.Index
-            });
-        } else if (stage == 3) {
-            this.log_obj.rounds.push({
-                attackCrit: false,
-                attackType: 16,
-                attackMissed: true,
-                attackSecondary: false,
-                attackDamage: 0,
-                attacker: this.a.Player.ID || this.a.Index,
-                target: this.b.Player.ID || this.b.Index
-            });
-        } else if (stage == 4) {
-            let [ source, target, weapon, damage, skipped, critical, extra ] = args;
-            this.log_obj.rounds.push({
-                attackCrit: critical,
-                attackType: (critical ? 1 : (skipped ? (target.Player.Class == WARRIOR ? 3 : 4) : 0)) + 10 * extra,
-                attackMissed: skipped,
-                attackDamage: damage,
-                attackSecondary: weapon != source.Weapon1,
-                attacker: source.Player.ID || source.Index,
-                target: target.Player.ID || target.Index
-            });
-        } else if (stage == 5) {
-            this.log_obj.rounds.push({
-                attackCrit: false,
-                attackType: 100,
-                attackMissed: false,
-                attackDamage: 0,
-                attackSecondary: false,
-                attacker: this.a.Player.ID || this.a.Index,
-                target: this.b.Player.ID || this.b.Index
-            });
-        }
     }
 }
