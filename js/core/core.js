abb39a7d7d79b4f7bfc03757a90a5b2e06585b0b
Martin <HafisCZ@users.noreply.github.com>
2022-08-27 21:26:37 +0100

    Advanced locale version

--- js/core/core.js
+++ js/core/core.js
@@ -2,7 +2,7 @@
 const MODULE_VERSION = 'v5.2250';
 const TABLE_VERSION = 'v10';
 const CORE_VERSION = 'v3.5';
-const LOCALES_VERSION = 'v3';
+const LOCALES_VERSION = 'v4';
 
 const Logger = new (class {
     constructor () {
