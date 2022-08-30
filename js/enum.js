384847811f455344a9c4d1d8d999c9205eba23af
Martin <HafisCZ@users.noreply.github.com>
2022-08-30 17:31:03 +0100

    Added translations to pet simulator

--- js/enum.js
+++ js/enum.js
@@ -36,14 +36,6 @@ const PLAYER_CLASS_SEARCH = [
     'bard'
 ];
 
-const PET_TYPE_NAMES = [
-    'Shadow',
-    'Light',
-    'Earth',
-    'Fire',
-    'Water'
-];
-
 const MAIN_ATTRIBUTE_MAP = {
     'Main': [ 'Strength', 'Intelligence', 'Dexterity', 'Dexterity', 'Strength', 'Strength', 'Dexterity', 'Intelligence', 'Intelligence' ],
     'Side1': [ 'Dexterity', 'Strength', 'Strength', 'Strength', 'Dexterity', 'Dexterity', 'Strength', 'Strength', 'Strength' ],
