de8bcbc28871b725bbc8b311ef73e18c47346292
Mathire <112318175+Mathire@users.noreply.github.com>
2022-11-17 10:35:59 +0000

    Updated italian and english translations (#47)

--- js/core/core.js
+++ js/core/core.js
@@ -3,7 +3,7 @@ const MODULE_VERSION_MAJOR = '5';
 const MODULE_VERSION = 'v5.2469';
 const TABLE_VERSION = 'v10';
 const CORE_VERSION = 'v3.5';
-const LOCALES_VERSION = 'v11';
+const LOCALES_VERSION = 'v11.1';
 
 const Logger = new (class {
     constructor () {
