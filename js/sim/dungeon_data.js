edb83c7ed7758c456a4317022032089a0a657ba8
Martin <HafisCZ@users.noreply.github.com>
2022-11-20 22:16:31 +0000

    Fixed berserker armor

--- js/sim/dungeon_data.js
+++ js/sim/dungeon_data.js
@@ -393,10 +393,10 @@ DUNGEON_DATA = {
             '4': { pos: 4, id: 1403, class: 1, level: 305, str: 15000, dex: 3500, int: 3500, con: 58500, lck: 8000, health: 101351256, runes: { type: 42, res: [15, 25, 60], damage: 30 }, min: 1067, max: 1829 },
             '5': { pos: 5, id: 1404, class: 1, level: 330, str: 12500, dex: 6000, int: 6000, con: 73500, lck: 9000, health: 137445008, runes: { type: 40, res: [60, 30, 30], damage: 40 }, min: 1155, max: 1979 },
             '6': { pos: 6, id: 1405, class: 4, level: 360, str: 6500, dex: 18500, int: 6500, con: 83500, lck: 11500, health: 135938000, runes: { type: 41, res: [40, 50, 50], damage: 45 }, min: 2524, max: 4319 },
-            '7': { pos: 7, id: 1406, class: 6, level: 390, str: 22500, dex: 6500, int: 6500, con: 81500, lck: 10500, health: 143440000, runes: { type: 41, res: [50, 50, 10], damage: 45 }, min: 1092, max: 2080 },
+            '7': { pos: 7, id: 1406, class: 6, level: 390, str: 22500, dex: 6500, int: 6500, con: 81500, lck: 10500, health: 143440000, runes: { type: 41, res: [50, 50, 10], damage: 45 }, min: 1092, max: 2080, armor: 17500 },
             '8': { pos: 8, id: 1407, class: 3, level: 420, str: 10500, dex: 22500, int: 10500, con: 112500, lck: 15500, health: 212850000, runes: { type: 42, res: [50, 50, 50], damage: 50 }, min: 1837, max: 3148 },
-            '9': { pos: 9, id: 1408, class: 6, level: 455, str: 29500, dex: 7000, int: 7000, con: 115000, lck: 16000, health: 235290000, runes: { type: 42, res: [50, 50, 50], damage: 55 }, min: 1296, max: 2441 },
-            '10': { pos: 10, id: 1409, class: 6, level: 500, str: 38500, dex: 10500, int: 10500, con: 158000, lck: 23000, health: 354552000, runes: { type: 40, res: [60, 50, 50], damage: 60 }, min: 1413, max: 2699 }
+            '9': { pos: 9, id: 1408, class: 6, level: 455, str: 29500, dex: 7000, int: 7000, con: 115000, lck: 16000, health: 235290000, runes: { type: 42, res: [50, 50, 50], damage: 55 }, min: 1296, max: 2441, armor: 17500 },
+            '10': { pos: 10, id: 1409, class: 6, level: 500, str: 38500, dex: 10500, int: 10500, con: 158000, lck: 23000, health: 354552000, runes: { type: 40, res: [60, 50, 50], damage: 60 }, min: 1413, max: 2699, armor: 17500 }
         }
     },
     '20': {
