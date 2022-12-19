73047dcafd2c8bdc54813ff878314790df02e35b
Martin <HafisCZ@users.noreply.github.com>
2022-12-19 22:17:57 +0000

    Moved shared debug code to separate file

---
+++ js/sim/debug_utils.js
@@ -0,0 +1,188 @@
+const SimulatorDebugDialog = new(class extends Dialog {
+  _intl_key () {
+      return 'simulator_debug';
+  }
+
+  _createModal () {
+      return `
+          <div class="ui basic small modal" style="background-color: #ffffff; padding: 1em; margin: -2em; border-radius: 0.5em; border: 1px solid #0b0c0c;">
+              <h3 class="ui header" style="color: black; padding-bottom: 0.5em; padding-top: 0; padding-left: 0;">${this.intl('title')}</h3>
+              <div style="overflow-y: scroll; overflow-x: hidden; max-height: 70vh; margin-bottom: 1.5em; margin-top: 1em; padding-right: 1em;">
+                  <div class="ui small form" style="line-height: 1.3em;" data-op="content"></div>
+              </div>
+              <div class="ui three fluid buttons">
+                  <button class="ui black fluid button" data-op="cancel">${this.intl('cancel')}</button>
+                  <button class="ui fluid button" data-op="reset">${this.intl('reset')}</button>
+                  <button class="ui fluid button" style="background-color: orange; color: black;" data-op="ok">${this.intl('ok')}</button>
+              </div>
+          </div>
+      `;
+  }
+
+  _createBindings () {
+      this.$cancelButton = this.$parent.find('[data-op="cancel"]');        
+      this.$cancelButton.click(() => {
+          this.close();
+      });
+
+      this.$okButton = this.$parent.find('[data-op="ok"]');
+      this.$okButton.click(() => {
+          this.callback(this._readData());
+          this.close();
+      })
+
+      this.$resetButton = this.$parent.find('[data-op="reset"]');
+      this.$resetButton.click(() => {
+          this._setData(this.defaultObject, this.defaultObject);
+      })
+
+      this.$content = this.$parent.find('[data-op="content"]');
+  }
+
+  _getValue (path, def) {
+      const value = this.$parent.find(`[data-path="${path}"]`).val()
+
+      if (typeof def === 'boolean') {
+          return value !== '0'
+      } else {
+          return parseFloat(value || def);
+      }
+  }
+
+  _convertValue (val) {
+      if (typeof val === 'boolean') {
+          return val ? '1' : '0';
+      } else {
+          return val;
+      }
+  }
+
+  _readData () {
+      const data = {};
+
+      for (const [group, groupItems] of Object.entries(this.defaultObject)) {
+          data[group] = {}
+
+          for (const [key, value] of Object.entries(groupItems)) {
+              if (Array.isArray(value)) {
+                  data[group][key] = [];
+
+                  for (let i = 0; i < value.length; i++) {
+                      data[group][key][i] = this._getValue(`${group}.${key}.${i}`, value[i]);
+                  }
+              } else {
+                  data[group][key] = this._getValue(`${group}.${key}`, value);
+              }
+          }
+      }
+
+      return data;
+  }
+
+  _setData (object, defaultObject) {
+      this.defaultObject = defaultObject;
+      this.object = object || defaultObject;
+
+      let content = '';
+      for (const [group, groupItems] of Object.entries(this.object)) {
+          content += `<h2 class="ui dividing header">${group}</h2>`;
+
+          for (const [key, value] of Object.entries(groupItems)) {
+              if (Array.isArray(value)) {
+                  content += '<div class="equal width fields">';
+                  for (let i = 0; i < value.length; i++) {
+                      content += `
+                          <div class="field">
+                              <label>${key} - ${i + 1}</label>
+                              <input type="number" data-path="${group}.${key}.${i}" value="${this._convertValue(value[i])}">
+                          </div>
+                      `;
+                  }
+  
+                  content += '</div>';
+              } else {
+                  content += `
+                      <div class="field">
+                          <label>${key}</label>
+                          <input type="number" data-path="${group}.${key}" value="${this._convertValue(value)}">
+                      </div>
+                  `;
+              }
+          }
+      }
+
+      this.$content.html(content);
+  }
+
+  _applyArguments (object, defaultObject, callback) {
+      this.callback = callback;
+
+      this._setData(object, defaultObject);
+  }
+})();
+
+const SimulatorUtils = new (class {
+  configure (normalConfig, debugMode = false) {
+    this.customConfig = null;
+    this.normalConfig = normalConfig;
+
+    if (debugMode) {
+      this._insertDebugElements()
+    }
+  }
+
+  _insertDebugElements () {
+    this.$button = $(`
+      <div class="item" id="simulate-configure" style="padding: 0;">
+        <button class="ui basic inverted icon button" data-position="bottom center" data-intl-tooltip="simulator.configure" style="box-shadow: none !important;"><i class="wrench icon"></i></button>
+      </div>
+    `).click(() => {
+      DialogController.open(
+          SimulatorDebugDialog,
+          this.customConfig,
+          this.normalConfig,
+          (config) => {
+            this.customConfig = config;
+            this._renderConfig();
+          }
+      );
+    });
+
+    this.$button.insertAfter($('.ui.menu.css-menu .css-a-blank').parent())
+  }
+
+  _renderConfig () {
+    if (typeof this.$display === 'undefined') {
+      this.$display = $('<div style="position: absolute; left: 2em; bottom: 2em; font-size: 90%;"></div>').appendTo($(document.body));
+    }
+
+    let content = '';
+    if (this.customConfig) {
+        for (const [type, value] of Object.entries(CONFIG)) {
+            const differences = [];
+            for (const [subtype, subvalue] of Object.entries(value)) {
+                const customValue = _dig(this.customConfig, type, subtype);
+
+                if (Array.isArray(subvalue) ? customValue.some((v, i) => v != subvalue[i]) : customValue != subvalue) {
+                    differences.push(`<div>${subtype}: <span style="color: red;">${subvalue}</span> -&gt; <span style="color: green;">${customValue}</span></div>`)
+                }
+            }
+
+            if (differences.length > 0) {
+                content += `<div><h4 style="margin-top: 0.5em; margin-bottom: 0;">${type}</h4>${differences.join('')}</div>`;
+            }
+        }
+    }
+
+    this.$display.html(content);
+  }
+
+  get config () {
+    return this.customConfig;
+  }
+
+  set config (value) {
+    this.customConfig = value;
+    this._renderConfig();
+  }
+})();
