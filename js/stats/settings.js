0ad892aad956731942ea46682603171e85e28b96
Martin <HafisCZ@users.noreply.github.com>
2023-01-13 22:18:32 +0000

    Removed script compat flags

--- js/stats/settings.js
+++ js/stats/settings.js
@@ -2954,7 +2954,7 @@ const Templates = new (class {
         this.keys.sort((a, b) => a.localeCompare(b));
     }
 
-    saveInternal (name, content, compat = { cm: false, cg: false, cp: false }) {
+    saveInternal (name, content) {
         // Check if a template already exists
         let exists = name in this.templates;
         let template = exists ? this.templates[name] : null;
@@ -2962,7 +2962,6 @@ const Templates = new (class {
         if (exists) {
             // Overwrite needed parts
             template.content = content;
-            template.compat = compat;
             template.version = MODULE_VERSION;
             template.timestamp = Date.now();
         } else {
@@ -2970,7 +2969,6 @@ const Templates = new (class {
             template = {
                 name: name,
                 content: content,
-                compat: compat,
                 version: MODULE_VERSION,
                 timestamp: Date.now(),
                 online: false
@@ -3009,11 +3007,11 @@ const Templates = new (class {
         }
     }
 
-    save (name, content, compat) {
+    save (name, content) {
         this.initialize();
 
         // Save template
-        this.saveInternal(name, content, compat);
+        this.saveInternal(name, content);
 
         // Commit changes
         this.commit();
