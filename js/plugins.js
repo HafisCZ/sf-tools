72814d035b076183db62fed584283968a5b07857
Martin <HafisCZ@users.noreply.github.com>
2023-03-19 22:20:52 +0000

    Added new numberVal helper

--- js/plugins.js
+++ js/plugins.js
@@ -3,6 +3,10 @@
         return this.find(`[data-op="${op}"]`);
     }
 
+    $.fn.numberVal = function () {
+        return Number(this.val());
+    }
+
     $.fn.searchfield = function (command, arg) {
         return this.each(function () {
             var $this = $(this);
