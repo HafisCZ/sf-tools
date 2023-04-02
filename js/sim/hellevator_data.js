d437582dff28af0a0fc338ec5330d653b3c1c2a5
Martin <HafisCZ@users.noreply.github.com>
2023-04-02 18:39:45 +0100

    Added proper hellevator damage ranges

--- js/sim/hellevator_data.js
+++ js/sim/hellevator_data.js
@@ -7,48 +7,48 @@ const HELLEVATOR_THEMES = [
 const HELLEVATOR_ENEMY_DELTA = [
 	{
     range: [0, 94],
-		reset: [100, 50, 100, 50, 2000],
-		delta: [40, 20, 40, 20, 4000]
+		reset: [100, 50, 100, 50, 2000, 50, 75],
+		delta: [40, 20, 40, 20, 4000, 4, 4]
 	},
 	{
     range: [95, 194],
-		reset: [5000, 2500, 5500, 2500, 1000000],
-		delta: [240, 120, 1000, 120, 3000000]
+		reset: [5000, 2500, 5500, 2500, 1000000, 500, 750],
+		delta: [240, 120, 1000, 120, 3000000, 8, 8]
 	},
 	{
     range: [195, 254],
-		reset: [30000, 15000, 110000, 15000, 300000000],
-		delta: [900, 500, 1600, 500, 6000000]
+		reset: [30000, 15000, 110000, 15000, 300000000, 1500, 1750],
+		delta: [900, 500, 1600, 500, 6000000, 10, 10]
 	},
 	{
     range: [245, 294],
-		reset: [80000, 40000, 190000, 40000, 600000000],
-		delta: [1000, 500, 1600, 500, 6000000]
+		reset: [80000, 40000, 190000, 40000, 600000000, 2000, 2250],
+		delta: [1000, 500, 1600, 500, 6000000, 10, 10]
 	},
 	{
     range: [295, 344],
-		reset: [140000, 70000, 270000, 65000, 900000000],
-		delta: [1100, 550, 1600, 500, 6000000]
+		reset: [140000, 70000, 270000, 65000, 900000000, 2500, 2750],
+		delta: [1100, 550, 1600, 500, 6000000, 10, 10]
 	},
 	{
     range: [345, 394],
-		reset: [210000, 105000, 350000, 90000, 1200000000],
-		delta: [1200, 600, 1600, 500, 6000000]
+		reset: [210000, 105000, 350000, 90000, 1200000000, 3000, 3250],
+		delta: [1200, 600, 1600, 500, 6000000, 10, 10]
 	},
 	{
     range: [395, 444],
-		reset: [280000, 140000, 430000, 115000, 1500000000],
-		delta: [2000, 1000, 1600, 500, 14000000]
+		reset: [280000, 140000, 430000, 115000, 1500000000, 3500, 3750],
+		delta: [2000, 1000, 1600, 500, 14000000, 10, 10]
 	},
 	{
     range: [445, 494],
-		reset: [400000, 200000, 510000, 140000, 2200000000],
-		delta: [2400, 1200, 1600, 500, 14000000]
+		reset: [400000, 200000, 510000, 140000, 2200000000, 4000, 4250],
+		delta: [2400, 1200, 1600, 500, 14000000, 10, 10]
 	},
   {
     range: [495, 499],
-    reset: [540000, 280000, 590000, 165000, 2900000000],
-    delta: [2600, 1400, 1600, 500, 14000000]
+    reset: [540000, 280000, 590000, 165000, 2900000000, 4500, 4750],
+    delta: [2600, 1400, 1600, 500, 14000000, 10, 10]
   }
 ];
 
@@ -57,9 +57,7 @@ const HellevatorEnemies = new (class {
     this._enemies = {}
   }
 
-  _generateEnemy({ floor, level, main, side, con, luck, health, element, klass }) {
-    const damage = (level + 1) * (klass == 1 ? 2 : (klass == 2 ? 4.5 : 2.5));
-
+  _generateEnemy({ floor, level, main, side, con, luck, health, element, klass, min, max }) {
     const obj = {
       Floor: floor,
       Level: level,
@@ -86,8 +84,8 @@ const HellevatorEnemies = new (class {
         Wpn1: {
           AttributeTypes: { 2: element },
           Attributes: { 2: 25 },
-          DamageMax: damage,
-          DamageMin: damage,
+          DamageMax: max,
+          DamageMin: min,
           HasEnchantment: false
         }
       }
@@ -111,7 +109,7 @@ const HellevatorEnemies = new (class {
 
     const themeData = await fetch(`/js/sim/hellevator/theme${theme}.json`).then((data) => data.json());
 
-    for (const { range: [ rangeStart, rangeEnd ], reset: [ resetMain, resetSide, resetCon, resetLuck, resetHealth ], delta: [ deltaMain, deltaSide, deltaCon, deltaLuck, deltaHealth ] } of HELLEVATOR_ENEMY_DELTA) {
+    for (const { range: [ rangeStart, rangeEnd ], reset: [ resetMain, resetSide, resetCon, resetLuck, resetHealth, resetMin, resetMax ], delta: [ deltaMain, deltaSide, deltaCon, deltaLuck, deltaHealth, deltaMin, deltaMax ] } of HELLEVATOR_ENEMY_DELTA) {
       for (let i = rangeStart; i <= rangeEnd; i++) {
         const delta = i - rangeStart;
 
@@ -124,13 +122,15 @@ const HellevatorEnemies = new (class {
           side: resetSide + delta * deltaSide, 
           con: resetCon + delta * deltaCon,
           luck: resetLuck + delta * deltaLuck,
-          health: resetHealth + delta * deltaHealth
+          health: resetHealth + delta * deltaHealth,
+          min: resetMin + delta * deltaMin,
+          max: resetMax + delta * deltaMax
         });
       }
     }
   }
 
-  async floorRange (start, end, theme) {
+  async floorRange (start, end = start, theme = HELLEVATOR_THEMES[0]) {
     if (typeof this._enemies[theme] === 'undefined') {
       await this._generateEnemies(theme);
     }
