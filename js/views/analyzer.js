f7e80ba14d1420e524a747cb5d8edfbd918fd125
Martin <HafisCZ@users.noreply.github.com>
2023-04-07 17:52:53 +0100

    Renamed item model

--- js/views/analyzer.js
+++ js/views/analyzer.js
@@ -270,8 +270,8 @@ class FighterModel {
       this.Class = dataType.long();
 
       this.Items = {
-          Wpn1: new SFItem(dataType.sub(12), 1, [1, 1]),
-          Wpn2: new SFItem(dataType.sub(12), 2, [1, 2])
+          Wpn1: new ItemModel(dataType.sub(12), 1, [1, 1]),
+          Wpn2: new ItemModel(dataType.sub(12), 2, [1, 2])
       }
 
       if (this.Face.Mouth < 0) {
