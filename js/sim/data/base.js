b8f49583b662fecef74cc6cc61a18a19698b7dae
Martin <HafisCZ@users.noreply.github.com>
2024-04-21 17:53:02 +0100

    Added monster generator class

---
+++ js/sim/data/base.js
@@ -0,0 +1,103 @@
+class MonsterGenerator {
+  static #DELTA = [
+    {
+      range: [10, 199],
+      reset: [100, 50, 100, 50, 2000, 50, 75],
+      delta: [20, 10, 20, 10, 2000, 2, 2]
+    },
+    {
+      range: [200, 399],
+      reset: [5000, 2500, 5500, 2500, 1000000, 500, 750],
+      delta: [120, 60, 500, 60, 1500000, 4, 4]
+    },
+    {
+      range: [400, 499],
+      reset: [30000, 15000, 110000, 15000, 300000000, 1500, 1750],
+      delta: [450, 250, 800, 250, 3000000, 5, 5]
+    },
+    {
+      range: [500, 599],
+      reset: [80000, 40000, 190000, 40000, 600000000, 2000, 2250],
+      delta: [500, 250, 800, 250, 3000000, 5, 5]
+    },
+    {
+      range: [600, 699],
+      reset: [140000, 70000, 270000, 65000, 900000000, 2500, 2750],
+      delta: [550, 275, 800, 250, 3000000, 5, 5]
+    },
+    {
+      range: [700, 799],
+      reset: [210000, 105000, 350000, 90000, 1200000000, 3000, 3250],
+      delta: [600, 300, 800, 250, 3000000, 5, 5]
+    },
+    {
+      range: [800, 899],
+      reset: [280000, 140000, 430000, 115000, 1500000000, 3500, 3750],
+      delta: [1000, 500, 800, 250, 7000000, 25, 25]
+    },
+    {
+      range: [900, 999],
+      reset: [400000, 200000, 510000, 140000, 2200000000, 6000, 6250],
+      delta: [1200, 600, 800, 250, 7000000, 25, 25]
+    },
+    {
+      range: [1000, 1009],
+      reset: [540000, 280000, 590000, 165000, 2900000000, 8500, 8750],
+      delta: [1300, 700, 800, 250, 7000000, 25, 25]
+    }
+  ]
+
+  static get (monsterLevel, monsterClass, monsterRuneType, monsterRuneValue) {
+    const base = this.#DELTA.find((entry) => monsterLevel >= entry.range[0] && monsterLevel <= entry.range[1]);
+
+    const delta = monsterLevel - base.range[0];
+    const [
+      resetMain, resetSide, resetCon, resetLuck, resetHealth, resetMin, resetMax
+    ] = base.reset;
+    const [
+      deltaMain, deltaSide, deltaCon, deltaLuck, deltaHealth, deltaMin, deltaMax
+    ] = base.delta;
+
+    const main = resetMain + deltaMain * delta;
+    const side = resetSide + deltaSide * delta;
+    const con = resetCon + deltaCon * delta;
+    const luck = resetLuck + deltaLuck * delta;
+    const health = resetHealth + deltaHealth * delta;
+    const min = resetMin + deltaMin * delta;
+    const max = resetMax + deltaMax * delta;
+
+    const model = {
+      NoBaseDamage: true,
+      Level: monsterLevel,
+      Class: monsterClass,
+      Armor: monsterLevel * CONFIG.fromIndex(monsterClass).MaximumDamageReduction,
+      Health: health,
+      Luck: { Total: luck },
+      Constitution: { Total: con },
+      Runes: {
+        Health: 0,
+        ResistanceFire: monsterRuneType == 40 ? monsterRuneValue : 0,
+        ResistanceCold: monsterRuneType == 41 ? monsterRuneValue : 0,
+        ResistanceLightning: monsterRuneType == 42 ? monsterRuneValue : 0
+      },
+      Items: {
+        Wpn1: {
+          AttributeTypes: { 2: monsterRuneType },
+          Attributes: { 2: monsterRuneValue },
+          DamageMax: max,
+          DamageMin: min
+        }
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      const name = PlayerModel.ATTRIBUTE_ORDER_BY_ATTRIBUTE[CONFIG.fromIndex(monsterClass).Attribute][i];
+
+      model[name] = {
+        Total: i === 0 ? main : side
+      }
+    }
+
+    return model;
+  }
+}
--- js/sim/data/hellevator.js
+++ js/sim/data/hellevator.js
@@ -7,122 +7,22 @@ class HellevatorEnemies {
     8, 7, 6, 5, 4, 3, 2, 1, 0
   ]
 
-  static HELLEVATOR_ENEMY_DELTA = [
-    {
-      range: [0, 94],
-      reset: [100, 50, 100, 50, 2000, 50, 75],
-      delta: [40, 20, 40, 20, 4000, 4, 4]
-    },
-    {
-      range: [95, 194],
-      reset: [5000, 2500, 5500, 2500, 1000000, 500, 750],
-      delta: [240, 120, 1000, 120, 3000000, 8, 8]
-    },
-    {
-      range: [195, 254],
-      reset: [30000, 15000, 110000, 15000, 300000000, 1500, 1750],
-      delta: [900, 500, 1600, 500, 6000000, 10, 10]
-    },
-    {
-      range: [245, 294],
-      reset: [80000, 40000, 190000, 40000, 600000000, 2000, 2250],
-      delta: [1000, 500, 1600, 500, 6000000, 10, 10]
-    },
-    {
-      range: [295, 344],
-      reset: [140000, 70000, 270000, 65000, 900000000, 2500, 2750],
-      delta: [1100, 550, 1600, 500, 6000000, 10, 10]
-    },
-    {
-      range: [345, 394],
-      reset: [210000, 105000, 350000, 90000, 1200000000, 3000, 3250],
-      delta: [1200, 600, 1600, 500, 6000000, 10, 10]
-    },
-    {
-      range: [395, 444],
-      reset: [280000, 140000, 430000, 115000, 1500000000, 3500, 3750],
-      delta: [2000, 1000, 1600, 500, 14000000, 50, 50]
-    },
-    {
-      range: [445, 494],
-      reset: [400000, 200000, 510000, 140000, 2200000000, 6000, 6250],
-      delta: [2400, 1200, 1600, 500, 14000000, 50, 50]
-    },
-    {
-      range: [495, 499],
-      reset: [540000, 280000, 590000, 165000, 2900000000, 8500, 8750],
-      delta: [2600, 1400, 1600, 500, 14000000, 50, 50]
-    }
-  ]
-
-  static #generateEnemy({ floor, level, main, side, con, luck, health, element, klass, min, max }) {
-    const obj = {
-      Floor: floor,
-      Level: level,
-      Class: klass,
-      NoBaseDamage: true,
-      Armor: level * CONFIG.fromIndex(klass).MaximumDamageReduction,
-      Health: health,
-      Potions: { Life: 0 },
-      Dungeons: {
-        Player: 0,
-        Group: 0
-      },
-      Luck: { Total: luck },
-      Constitution: { Total: con },
-      Fortress: { Gladiator: 0 },
-      Runes: {
-        Health: 0,
-        ResistanceFire: element == 40 ? 25 : 0,
-        ResistanceCold: element == 41 ? 25 : 0,
-        ResistanceLightning: element == 42 ? 25 : 0
-      },
-      Items: {
-        Hand: {},
-        Wpn1: {
-          AttributeTypes: { 2: element },
-          Attributes: { 2: 25 },
-          DamageMax: max,
-          DamageMin: min,
-          HasEnchantment: false
-        }
-      }
-    };
-
-    for (let i = 0; i < 3; i++) {
-      const name = PlayerModel.ATTRIBUTE_ORDER_BY_ATTRIBUTE[CONFIG.fromIndex(klass).Attribute][i];
-
-      obj[name] = {
-        Total: i === 0 ? main : side
-      }
-    }
-
-    return obj;
-  }
-
   static async #generateEnemies (theme) {
     this.#enemies[theme] = [];
 
     const themeData = await fetch(`/js/playa/hellevator/theme${theme}.json`).then((data) => data.json());
 
-    for (const { range: [ rangeStart, rangeEnd ], reset: [ resetMain, resetSide, resetCon, resetLuck, resetHealth, resetMin, resetMax ], delta: [ deltaMain, deltaSide, deltaCon, deltaLuck, deltaHealth, deltaMin, deltaMax ] } of this.HELLEVATOR_ENEMY_DELTA) {
-      for (let i = rangeStart; i <= rangeEnd; i++) {
-        const delta = i - rangeStart;
+    for (let i = 0; i < 500; i++) {
+      const monster = MonsterGenerator.get(
+        10 + i * 2,
+        _dig(themeData, i, 1) || 1,
+        _dig(themeData, i, 0) || 40,
+        25
+      )
 
-        this.#enemies[theme][i] = this.#generateEnemy({
-          floor: i + 1,
-          level: 10 + i * 2,
-          klass: _dig(themeData, i, 1) || 1,
-          element: _dig(themeData, i, 0) || 40,
-          main: resetMain + delta * deltaMain,
-          side: resetSide + delta * deltaSide, 
-          con: resetCon + delta * deltaCon,
-          luck: resetLuck + delta * deltaLuck,
-          health: resetHealth + delta * deltaHealth,
-          min: resetMin + delta * deltaMin,
-          max: resetMax + delta * deltaMax
-        });
-      }
+      monster.Floor = i + 1;
+
+      this.#enemies[theme][i] = monster;
     }
   }
 
