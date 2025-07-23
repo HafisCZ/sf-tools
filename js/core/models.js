a7c99f0e284b71b1a5735a7c297537b8f34ef700
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2025-07-23 12:25:45 +0200

    update scrapbook count

--- js/core/models.js
+++ js/core/models.js
@@ -801,7 +801,7 @@ class DungeonHelper {
 
 class PlayerModel {
     static ACHIEVEMENTS_COUNT = 104;
-    static SCRAPBOOK_COUNT = 2315;
+    static SCRAPBOOK_COUNT = 2374;
 
     static SCROLL_MAP = {
         11: 0,
