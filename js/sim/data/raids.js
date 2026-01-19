30039d0765fe6a21aba5f5c98af57e4e865894a1
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2026-01-19 23:44:48 +0100

    add raid 100 with 10x stats

--- js/sim/data/raids.js
+++ js/sim/data/raids.js
@@ -944,6 +944,25 @@ const RAID_DATA = {
       '14': { pos: 14, id: 0, class: 1, level: 400, str: 14897, dex: 5227, int: 5173, con: 54291, lck: 3419, health: 108853456, min: 366, max: 624 },
       '15': { pos: 15, id: 0, class: 1, level: 400, str: 14897, dex: 5227, int: 5173, con: 135727, lck: 3419, health: 272132640, min: 366, max: 624 }
     }
+  },
+  'raid_100': {
+    floors: {
+      '1': { pos: 1, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '2': { pos: 2, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '3': { pos: 3, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '4': { pos: 4, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '5': { pos: 5, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '6': { pos: 6, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '7': { pos: 7, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '8': { pos: 8, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '9': { pos: 9, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '10': { pos: 10, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '11': { pos: 11, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 217270, lck: 34190, health: 544257240, min: 465, max: 825 },
+      '12': { pos: 12, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 325910, lck: 34190, health: 816395920, min: 465, max: 825 },
+      '13': { pos: 13, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 325910, lck: 34190, health: 816395920, min: 465, max: 825 },
+      '14': { pos: 14, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 434540, lck: 34190, health: 1088534560, min: 465, max: 825 },
+      '15': { pos: 15, id: 0, class: 1, level: 500, str: 148970, dex: 52270, int: 51730, con: 5431790, lck: 34190, health: 13606631680, min: 465, max: 825 }
+    }
   }
 }
 
