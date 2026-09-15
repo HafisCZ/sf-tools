57bf52dfcb9ca3624b58499f96afc6198c31febd
Martin <HafisCZ@users.noreply.github.com>
2026-09-15 18:19:35 +0200

    Removed unused map

--- js/core/models.js
+++ js/core/models.js
@@ -861,8 +861,6 @@ class DungeonHelper {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36
     ];
 
-    static PLAYA_TO_INTERNAL_MAPPING_ENTRIES = Object.entries(DungeonHelper.PLAYA_TO_INTERNAL_MAPPING);
-
     static LIGHT_DUNGEON_COUNT = DungeonHelper.PLAYA_TO_INTERNAL_MAPPING.length;
 
     static SHADOW_DUNGEON_COUNT = DungeonHelper.PLAYA_TO_INTERNAL_MAPPING.length;
